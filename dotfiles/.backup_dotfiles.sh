ab8c17189e7649450eba03728fccc3d48adde53e
Ebinezar Dhanaraj <ebiiscoding@gmail.com>
2026-07-07 03:09:49 +0530

    Auto backup: dotfiles 2026-07-07 03:09

--- dotfiles/.backup_dotfiles.sh
+++ dotfiles/.backup_dotfiles.sh
@@ -10,7 +10,7 @@ CHECK="${GREEN}✔${RESET}"
 CROSS="${RED}✘${RESET}"
 ARROW="${PURPLE}❯${RESET}"
 
-REPO="$HOME/Documents/Ebi Workspace (Backup)/terminal-ebi-setup"
+REPO="$HOME/terminal-ebi-setup"
 DOTFILES_DIR=$REPO/dotfiles
 LOG_FILE=~/.logs/backup_dotfiles.log
 mkdir -p ~/.logs
